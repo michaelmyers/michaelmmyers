14a5646854553b033358104c84eb3b2a3d3718f6
Michael Myers <michael.morris.myers@gmail.com>
2013-10-14 21:40:46 -0400

    added facebook, cleaned up comments, added iphone

--- js/main.js
+++ js/main.js
@@ -3,22 +3,11 @@
 $(document).ready(function() {
     $.fn.fullpage({
         slidesColor: ['#34495e', '#34495e', '#34495e'],
-        anchors: ['home', 'about', 'novum-io', 'robopogo'],
+        anchors: ['home', 'about', 'novum-io', 'robopogo', 'dc-transit'],
         verticalCentered: false,
         resize: false
     });
 });
-
-/*
-$(".main").onepage_scroll({
-    sectionContainer: "section", // sectionContainer accepts any kind of selector in case you don't want to use section
-    easing: "ease", // Easing options accepts the CSS3 easing animation such "ease", "linear", "ease-in", "ease-out", "ease-in-out", or even cubic bezier value such as "cubic-bezier(0.175, 0.885, 0.420, 1.310)"
-    animationTime: 1000, // AnimationTime let you define how long each section takes to animate
-    pagination: true, // You can either show or hide the pagination. Toggle true for show, false for hide.
-    updateURL: true // Toggle this true if you want the URL to be updated automatically when the user scroll to each page.
-}); */
-
-
 
 $(document).ready(function () {
 
