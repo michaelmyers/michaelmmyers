ecbed0c8addc081cab85d3662f5f3ac6bacf0330
Michael Myers <michael.morris.myers@gmail.com>
2013-10-15 22:07:05 -0400

    ready for prod

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@
 $(document).ready(function() {
     $.fn.fullpage({
         slidesColor: ['#34495e', '#34495e', '#34495e'],
-        anchors: ['home', 'about', 'novum-io', 'robopogo', 'dc-transit'],
+        anchors: ['home', 'about', 'novum-io', 'robopogo'],//, 'dc-transit'],
         verticalCentered: false,
         resize: false
     });
